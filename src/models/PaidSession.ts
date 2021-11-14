5f563eb84bf77635d5f8fb21f1f8697fe055bdf6
Calvin Koepke <hello@calvinkoepke.com>
2021-11-14 16:18:24 -0700

    fix: session type

--- src/models/PaidSession.ts
+++ src/models/PaidSession.ts
@@ -1,21 +1,43 @@
 import { NewAddress } from "../helpers/wallet/cardano";
 import { BaseModel } from "./BaseModel";
 
-export class PaidSession extends BaseModel {
-    public phoneNumber: string;
-    public cost: number;
-    public handle: string;
-    public wallet: NewAddress;
-    public start: number;
-    public txId?: string;
+type PaidSessionStatusType = "pending" | "submitted" | "confirmed";
 
-    constructor({ phoneNumber, cost, handle, wallet, start, txId }: { phoneNumber: string, cost: number, handle: string, wallet: NewAddress, start: number, txId?: string }) {
-        super();
-        this.phoneNumber = phoneNumber;
-        this.cost = cost;
-        this.handle = handle;
-        this.wallet = wallet;
-        this.start = start;
-        this.txId = txId;
-    }
+interface PaidSessionType {
+  phoneNumber: string;
+  cost: number;
+  handle: string;
+  wallet: NewAddress;
+  start: number;
+  txId?: string;
+  status?: PaidSessionStatusType
+}
+
+export class PaidSession extends BaseModel {
+  public phoneNumber: string;
+  public cost: number;
+  public handle: string;
+  public wallet: NewAddress;
+  public start: number;
+  public txId?: string;
+  public status?: PaidSessionStatusType;
+
+  constructor({
+    phoneNumber,
+    cost,
+    handle,
+    wallet,
+    start,
+    txId,
+    status = 'pending'
+  }: PaidSessionType) {
+    super();
+    this.phoneNumber = phoneNumber;
+    this.cost = cost;
+    this.handle = handle;
+    this.wallet = wallet;
+    this.start = start;
+    this.txId = txId;
+    this.status = status;
+  }
 }
