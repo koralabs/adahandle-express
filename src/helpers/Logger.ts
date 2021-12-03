7de671457a802e7391f8af2a3e9193db9cde7447
happygaijin <jlanderson@gmail.com>
2021-12-02 16:04:17 -0800

    Adding timestamps

--- src/helpers/Logger.ts
+++ src/helpers/Logger.ts
@@ -11,11 +11,15 @@ export enum LogCategory {
 export class Logger {
     public static log(args: { message: string, category?: LogCategory, event?: string } | string): void {
         if (typeof args === 'string') {
-            console.log(`{ "category": "${LogCategory.INFO}", "message": ${args}" }`);
+            this.log_entry(LogCategory.INFO, args);
             return;
         }
-
         const { message, category, event } = args;
-        console.log(`{ "category": "${category ?? LogCategory.INFO}", "message": "${message}", "event: ${event}" }`);
+        this.log_entry(category ?? LogCategory.INFO, message, event);
+    }
+
+    private static log_entry(category: LogCategory, message: string, event?: string): void {
+        const now = (new Date()).toISOString();
+        console.log(`{ "category": "${category ?? LogCategory.INFO}", "message": "${message}", "event: ${event}" , "timestamp": "${now}" }`);
     }
 }
