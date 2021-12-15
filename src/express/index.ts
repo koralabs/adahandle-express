f99bd9a2603a550eaf0ae0d6c4bc74e525d4cb54
Calvin Koepke <2815568+cjkoepke@users.noreply.github.com>
2021-12-15 12:38:19 -0700

    fix: remove sg client

--- src/express/index.ts
+++ src/express/index.ts
@@ -2,7 +2,6 @@ import * as express from "express";
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 import * as sgMail from '@sendgrid/mail';
-import * as sgClient from '@sendgrid/client';
 import { urlencoded, json } from 'body-parser'
 import Router from 'express-promise-router';
 
@@ -23,7 +22,6 @@ import { mintConfirmHandler } from "./handlers/jobs/mintConfirm";
 
 export const startServer = async () => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
-  sgClient.setApiKey(process.env.SENDGRID_VERIFY_KEY as string);
   const app = express();
   const router = Router();
 
