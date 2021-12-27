7c90fc5111c8edbfad694b83fef68af4582f06e8
happygaijin <jlanderson@gmail.com>
2021-12-27 15:13:05 -0800

    Excluding src/scripts from compiling

--- src/scripts/loadTesting/ipfsImageTesting.ts
+++ src/scripts/loadTesting/ipfsImageTesting.ts
@@ -8,7 +8,7 @@ const requestsPerMinute = 300;
     try {
         for(let i=0;i<10000;i++){
             console.log(`processing #${i}`);
-            getIPFSImage(i.toString(), false, 0, 0).then((ipfs) => {console.log(`ipfs for #${i}==${ipfs}`)});
+            getIPFSImage(i.toString()).then((ipfs) => {console.log(`ipfs for #${i}==${ipfs}`)});
             await delay((60/requestsPerMinute)*1000);
         }
     } catch(e) {
