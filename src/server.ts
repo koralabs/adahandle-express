fe4a1e44eb8f3f4d3f1be457f27d173931891f19
happygaijin <jlanderson@gmail.com>
2022-02-13 20:35:35 -0800

    removing console.log

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ import { startServer } from './express';
 import { Logger, LogCategory } from './helpers/Logger'
 import { Firebase } from './helpers/firebase';
 
-console.log(`SMTP_HOST=${process.env.SMTP_HOST}`);
 const startApp = async () => {
   // If either is set to production, they both should be production or exit  
   if (process.env.NODE_ENV?.trim() === 'production' || process.env.Machine_Environment?.trim() === 'production'){
