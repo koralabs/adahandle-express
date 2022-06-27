a33693942dc95d4abf72c7b39c9aa541aedd28f9
guavajellyaaron <aaron@guavajellyco.com>
2022-06-27 15:16:49 -0700

    fix tests

--- src/helpers/image.test.ts
+++ src/helpers/image.test.ts
@@ -11,71 +11,66 @@ jest.mock('../models/firestore/collections/ReservedHandles');
 jest.mock('node-html-to-image');
 
 describe('Image Helper Tests', () => {
+    const sessions: ActiveSession[] = [
+        new ActiveSession({
+            emailAddress: '',
+            cost: 0,
+            handle: 'burritos',
+            paymentAddress: '',
+            start: 0,
+            createdBySystem: CreatedBySystem.UI
+        }),
+        new ActiveSession({
+            emailAddress: '',
+            cost: 0,
+            handle: 'tacos',
+            paymentAddress: '',
+            start: 0,
+            createdBySystem: CreatedBySystem.UI
+        })
+    ];
 
-  const sessions: ActiveSession[] = [
-    new ActiveSession({
-      emailAddress: '',
-      cost: 0,
-      handle: 'burritos',
-      paymentAddress: '',
-      start: 0,
-      createdBySystem: CreatedBySystem.UI
-    }),
-    new ActiveSession({
-      emailAddress: '',
-      cost: 0,
-      handle: 'tacos',
-      paymentAddress: '',
-      start: 0,
-      createdBySystem: CreatedBySystem.UI
-    })
-  ]
+    describe('createNFTImages', () => {
+        it('should create NFT images', async () => {
+            jest.spyOn(ReservedHandles, 'getReservedHandles').mockResolvedValue({
+                twitter: [{ handle: 'burritos', index: 1 }, { handle: 'tacos' }],
+                protected: [],
+                private: [],
+                spos: []
+            });
 
-  describe('createNFTImages', () => {
-    it('should create NFT images', async () => {
-      jest.spyOn(ReservedHandles, 'getReservedHandles').mockResolvedValue({
-        twitter: [{ handle: 'burritos', index: 1 }, { handle: 'tacos' }],
-        protected: [],
-        private: [],
-        spos: []
-      });
+            const htmlToImageSpy = mocked(htmlToImage);
 
-      const htmlToImageSpy = mocked(htmlToImage);
+            await createNFTImages(sessions.map((session) => session.handle));
 
-      await createNFTImages(sessions);
+            // Test with index to see the og === true
+            expect(htmlToImageSpy).toHaveBeenNthCalledWith(1, {
+                content: expect.arrayContaining([
+                    expect.objectContaining({
+                        handle: 'burritos',
+                        og: true,
+                        ogNumber: 1,
+                        ogTotal: 2438,
+                        output: expect.stringContaining('burritos.jpg')
+                    })
+                ]),
+                html: expect.any(String),
+                quality: 100,
+                type: 'jpeg'
+            });
 
-      // Test with index to see the og === true
-      expect(htmlToImageSpy).toHaveBeenNthCalledWith(1, {
-        content: expect.arrayContaining([
-          expect.objectContaining(
-            {
-              "handle": "burritos",
-              "og": true,
-              "ogNumber": 1,
-              "ogTotal": 2438,
-              "output": expect.stringContaining("burritos.jpg"),
-            }
-          )
-        ]),
-        html: expect.any(String),
-        quality: 100,
-        type: 'jpeg'
-      });
-
-      // Test without index to see og removed
-      expect(htmlToImageSpy).toHaveBeenNthCalledWith(2, {
-        content: expect.arrayContaining([
-          expect.objectContaining(
-            {
-              "handle": "tacos",
-              "output": expect.stringContaining("tacos.jpg"),
-            }
-          )
-        ]),
-        html: expect.any(String),
-        quality: 100,
-        type: 'jpeg'
-      });
+            // Test without index to see og removed
+            expect(htmlToImageSpy).toHaveBeenNthCalledWith(2, {
+                content: expect.arrayContaining([
+                    expect.objectContaining({
+                        handle: 'tacos',
+                        output: expect.stringContaining('tacos.jpg')
+                    })
+                ]),
+                html: expect.any(String),
+                quality: 100,
+                type: 'jpeg'
+            });
+        });
     });
-  });
 });
