0d2d939133ea899ccdf9182eccab3747f2f35ae5
guavajellyaaron <aaron@guavajellyco.com>
2022-07-25 13:05:17 -0700

    Add eternlFeeAddress and eternlFee to active sessions

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,74 +1,72 @@
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
-import * as jwt from "jsonwebtoken";
+import * as jwt from 'jsonwebtoken';
 import { Secret } from 'jsonwebtoken';
-import { getS3 } from "./aws";
-import { LogCategory, Logger } from "../helpers/Logger";
+import { getS3 } from './aws';
+import { LogCategory, Logger } from '../helpers/Logger';
 
 export interface AccessJWTPayload extends jwt.JwtPayload {
-  emailAddress: string;
-  isSPO?: boolean;
+    emailAddress: string;
+    isSPO?: boolean;
 }
 
 export interface SessionJWTPayload extends jwt.JwtPayload {
-  emailAddress: string;
-  cost: number;
-  handle: string;
-  isSPO?: boolean;
+    emailAddress: string;
+    cost: number;
+    handle: string;
+    isSPO?: boolean;
+    eternlFeeAddress?: string;
 }
 
 export interface AllSessionsJWTPayload extends jwt.JwtPayload {
-  sessions: { handle: string; dateAdded: number, address?: string }[]
+    sessions: { handle: string; dateAdded: number; address?: string }[];
 }
 
-type SecretContext = 'access' | 'session'
+type SecretContext = 'access' | 'session';
 
 let accessSecret: Secret;
 let sessionSecret: Secret;
 
 export const getKey = async (
-  context: SecretContext = 'access',
-  type: 'secret' | 'public' = 'secret'
+    context: SecretContext = 'access',
+    type: 'secret' | 'public' = 'secret'
 ): Promise<Secret | null> => {
-  const s3 = getS3();
+    const s3 = getS3();
 
-  if ('access' === context && accessSecret) {
-    return accessSecret;
-  }
+    if ('access' === context && accessSecret) {
+        return accessSecret;
+    }
 
-  if ('session' === context && sessionSecret) {
-    return sessionSecret;
-  }
+    if ('session' === context && sessionSecret) {
+        return sessionSecret;
+    }
 
-  let Key = process.env[`MY_AWS_TOKEN_KEY_${context.toUpperCase()}`];
-  if ('public' === type) {
-    Key += '.pub';
-  }
+    let Key = process.env[`MY_AWS_TOKEN_KEY_${context.toUpperCase()}`];
+    if ('public' === type) {
+        Key += '.pub';
+    }
 
-  let res: GetObjectOutput;
-  try {
-    res = await s3
-      .getObject({
-        Bucket: process.env.MY_AWS_BUCKET || "",
-        Key: Key || "",
-      })
-      .promise();
-  } catch (e) {
-    Logger.log({ message: JSON.stringify(e), category: LogCategory.ERROR });
-    return null;
-  }
+    let res: GetObjectOutput;
+    try {
+        res = await s3
+            .getObject({
+                Bucket: process.env.MY_AWS_BUCKET || '',
+                Key: Key || ''
+            })
+            .promise();
+    } catch (e) {
+        Logger.log({ message: JSON.stringify(e), category: LogCategory.ERROR });
+        return null;
+    }
 
-  const secret = res?.Body
-    ? res.Body.toString("utf-8") as Secret
-    : null;
+    const secret = res?.Body ? (res.Body.toString('utf-8') as Secret) : null;
 
-  if ('access' === context && secret) {
-    accessSecret = secret;
-  }
+    if ('access' === context && secret) {
+        accessSecret = secret;
+    }
 
-  if ('session' === context && secret) {
-    sessionSecret = secret;
-  }
+    if ('session' === context && secret) {
+        sessionSecret = secret;
+    }
 
-  return secret;
-}
-
+    return secret;
+};
--- src/models/ActiveSession.ts
+++ src/models/ActiveSession.ts
@@ -1,4 +1,4 @@
-import { BaseModel } from "./BaseModel";
+import { BaseModel } from './BaseModel';
 import { CreatedBySystem } from '../helpers/constants';
 
 export enum Status {
@@ -13,28 +13,30 @@ export enum WorkflowStatus {
     PROCESSING = 'processing',
     SUBMITTED = 'submitted',
     CONFIRMED = 'confirmed',
-    EXPIRED = 'expired',
+    EXPIRED = 'expired'
 }
 
 export interface ActiveSessionInput {
-    emailAddress: string,
-    cost: number,
-    refundAmount?: number,
-    handle: string,
-    paymentAddress: string,
-    returnAddress?: string,
-    txHash?: string,
-    index?: number,
-    start: number,
-    id?: string,
-    txId?: string,
-    createdBySystem: CreatedBySystem,
-    status?: Status,
-    workflowStatus?: WorkflowStatus,
+    emailAddress: string;
+    cost: number;
+    refundAmount?: number;
+    handle: string;
+    paymentAddress: string;
+    returnAddress?: string;
+    txHash?: string;
+    index?: number;
+    start: number;
+    id?: string;
+    txId?: string;
+    createdBySystem: CreatedBySystem;
+    status?: Status;
+    workflowStatus?: WorkflowStatus;
     attempts?: number;
     dateAdded?: number;
     walletId?: string;
     ipfsHash?: string;
+    eternlFeeAddress?: string;
+    eternlFee?: number;
 }
 
 export class ActiveSession extends BaseModel {
@@ -49,15 +51,38 @@ export class ActiveSession extends BaseModel {
     public start: number;
     public id?: string;
     public txId?: string;
-    public createdBySystem: CreatedBySystem
-    public status?: Status
+    public createdBySystem: CreatedBySystem;
+    public status?: Status;
     public workflowStatus?: WorkflowStatus;
     public attempts?: number;
     public dateAdded?: number;
     public walletId?: string;
     public ipfsHash?: string;
+    public eternlFeeAddress?: string;
+    public eternlFee?: number;
 
-    constructor({ id, walletId, emailAddress, cost, refundAmount, handle, paymentAddress, start, txId, createdBySystem, returnAddress, txHash, index, workflowStatus, status = Status.PENDING, attempts = 0, dateAdded = Date.now(), ipfsHash }: ActiveSessionInput) {
+    constructor({
+        id,
+        walletId,
+        emailAddress,
+        cost,
+        refundAmount,
+        handle,
+        paymentAddress,
+        start,
+        txId,
+        createdBySystem,
+        returnAddress,
+        txHash,
+        index,
+        workflowStatus,
+        status = Status.PENDING,
+        attempts = 0,
+        dateAdded = Date.now(),
+        ipfsHash,
+        eternlFeeAddress,
+        eternlFee
+    }: ActiveSessionInput) {
         super();
         this.id = id;
         this.walletId = walletId;
@@ -77,5 +102,7 @@ export class ActiveSession extends BaseModel {
         this.attempts = attempts;
         this.dateAdded = dateAdded;
         this.ipfsHash = ipfsHash;
+        this.eternlFeeAddress = eternlFeeAddress;
+        this.eternlFee = eternlFee;
     }
 }
